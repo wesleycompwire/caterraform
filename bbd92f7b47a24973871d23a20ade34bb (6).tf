a2d317a5e11f3add98c951da18ca35683c14e34e
wesleycompwire <wesley.lopes@compwire.com.br>
2025-12-01 09:11:14 -0300

    Update bbd92f7b47a24973871d23a20ade34bb (6).tf

--- bbd92f7b47a24973871d23a20ade34bb (6).tf
+++ bbd92f7b47a24973871d23a20ade34bb (6).tf
@@ -2,7 +2,6 @@
 terraform {
   required_version = ">=1.0.0, <2.0"
 
-  # Define the required providers and their sources.
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
@@ -11,25 +10,23 @@ terraform {
 }
 
 locals {
-  # Define the IAM ID
   iam_id = "iam-ServiceId-6bf7af4e-6f07-4894-ab72-ff539dfb951a"
 }
 
-# Declare a variable for the IBM Cloud API key.
 variable "ibmcloud_api_key" {}
 
-# Define the IBM provider configuration and set the API key from the variable.
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
 }
 
-# Define a custom IAM role for Cloud Storage with specific actions.
+# Custom role for COS
 resource "ibm_iam_custom_role" "cos_custom_role" {
   name         = "CloudabilityStorageCustomRole"
   display_name = "CloudabilityStorageCustomRole"
   description  = "This is a custom role to read Cloud Storage"
   service      = "cloud-object-storage"
-  actions      = [
+
+  actions = [
     "iam.policy.read",
     "cloud-object-storage.object.head",
     "cloud-object-storage.object.get_uploads",
@@ -40,25 +37,33 @@ resource "ibm_iam_custom_role" "cos_custom_role" {
   ]
 }
 
+# IAM Service Policy for Cloud Object Storage
 resource "ibm_iam_service_policy" "storage_policy" {
-  iam_id         = local.iam_id
-  roles         = [ibm_iam_custom_role.cos_custom_role.display_name]
+  iam_id = local.iam_id
+
+  roles = [
+    ibm_iam_custom_role.cos_custom_role.display_name
+  ]
+
   resource_attributes {
-    name  = "resource"
-    value = "daily-cost-exports-cloudability"
+    name     = "resource"
+    value    = "daily-cost-exports-cloudability"
     operator = "stringEquals"
   }
+
   resource_attributes {
     name  = "serviceName"
     value = "cloud-object-storage"
   }
 }
 
-# Create another IAM access group policy for a different service with viewer role.
+# IAM Service Policy for Billing (existing one must be imported)
 resource "ibm_iam_service_policy" "billing_policy" {
-  iam_id         =  local.iam_id
-  roles           = ["Viewer"]
-  resources {
-    service = "billing"
+  iam_id = local.iam_id
+  roles  = ["Viewer"]
+
+  resource_attributes {
+    name  = "serviceName"
+    value = "billing"
   }
 }
